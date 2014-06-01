ccf6ebd6c6e61f3a204220560aa26636b998f968
FreemanCoding <t3wschool@gmail.com>
2014-06-01 17:39:30 -0400

    FIxed doge sayings popping up

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -4,20 +4,9 @@ function HTMLActuator() {
   this.bestContainer    = document.querySelector(".best-container");
   this.messageContainer = document.querySelector(".game-message");
   this.info             = document.querySelector(".info");  
-  this.dogeSays = document.querySelector(".doge-says");
-  this.adSpace = document.querySelector(".shout-out");
-
   this.score = 0;
 }
 
-var dogeSayings = ['such good', 'so amaze', 'many points', 'very unstoppable', 'great jorb', 'such playing', 'very good', 'points', 'very gaming', 'such player', 'concern' ,'bewildered',
-'many game', 'so good', 'very scores', 'so scoring', 'so hot right now', 'such playing', 'such matching', 'so matched', 'very matched', 'very neat' ,'such natural',]
-
-var ads = [
-  '<a href="http://snackcompass.com/" target="_blank">Like Pizza?</a>',
-  '<a href="http://maxhash.com/doge" target="_blank">Check out everything hashtagged Doge!</a>',
-  '<a href="http://peepe.rs/" target="_blank">Googly Eyes! Googly Eyes!</a>',  
-]
 
 HTMLActuator.prototype.actuate = function (grid, metadata) {
   var self = this;
@@ -117,8 +106,6 @@ HTMLActuator.prototype.positionClass = function (position) {
 
 HTMLActuator.prototype.updateScore = function (score) {
   this.clearContainer(this.scoreContainer);
-  this.clearContainer(this.dogeSays)
-
   var difference = score - this.score;
   this.score = score;
 
@@ -129,20 +116,7 @@ HTMLActuator.prototype.updateScore = function (score) {
     addition.classList.add("score-addition");
     addition.textContent = "+" + difference;
     this.scoreContainer.appendChild(addition);
-    
-    var message = dogeSayings[Math.floor(Math.random() * dogeSayings.length)]
-    var messageElement = document.createElement("p");
-    messageElement.textContent = message
-    var left = 'left:' + Math.round(Math.random() * 80) + '%;'
-    var top = 'top:' + Math.round(Math.random() * 80) + '%;'
-    var color = 'color: rgb(' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ');'
-    var styleString = left + top + color
-    messageElement.setAttribute('style', styleString);
-    this.dogeSays.appendChild(messageElement);
-    if (difference > 4) {
-     this.adSpace.innerHTML = ads[Math.floor(Math.random() * ads.length)]
-    }
-    
+       
   }
 };
 
